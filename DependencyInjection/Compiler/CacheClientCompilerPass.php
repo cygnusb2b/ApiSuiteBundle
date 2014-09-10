c442f9923a4a3f8dbe2c349898e6f95c81a4f3b3
Jacob Bare <jacob.bare@cygnus.com>
2014-09-09 21:56:59 -0500

    BASE-424 Fixed issue so cache clients were not dependent on other bundles

---
+++ DependencyInjection/Compiler/CacheClientCompilerPass.php
@@ -0,0 +1,37 @@
+<?php
+namespace Cygnus\ApiSuiteBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class CacheClientCompilerPass implements CompilerPassInterface
+{
+    /**
+     * The tag name used for API resources
+     */
+    const TAG_NAME = 'cygnus_api_suite.cacheable';
+
+    /**
+     * Adds tagged cacheable clients
+     *
+     * @param   Symfony\Component\DependencyInjection\ContainerBuilder $container
+     * @return  void
+     */
+    public function process(ContainerBuilder $container)
+    {
+        // Get the tagged resources
+        $resources = $container->findTaggedServiceIds(static::TAG_NAME);
+
+        $cacheClientId = $container->getParameter('cygnus_api_suite.cache_client');
+
+        foreach ($resources as $id => $tagAttributes) {
+            $definition = $container->getDefinition($id);
+
+            $definition->addMethodCall(
+                'setCacheClient',
+                [new Reference($cacheClientId)]
+            );
+        }
+    }
+}
