81b12c3e9488e51d0f9e7017d9602f8d58b5eb71
Josh Worden <josh.worden@cygnus.com>
2015-01-23 09:03:52 -0600

    BASE-985 Changed PUT to use pathname attribute

--- RemoteKernel/Curl/Client.php
+++ RemoteKernel/Curl/Client.php
@@ -334,7 +334,7 @@ class Client
 
             $this->addOption(CURLOPT_PUT, TRUE);
 
-            $filePath = sprintf("%s/%s", $file->getPath(), $file->getFilename());
+            $filePath = $file->getPathname();
 
             $fp = fopen($filePath, 'r');
 
