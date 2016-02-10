7d117ed791e2776116482933cfc0202d6c00e5b3
Jacob Bare <jacob.bare@cygnus.com>
2016-02-10 12:00:59 -0600

    Added additional error message parsing

--- ApiClient/ExactTarget/Exception/ResponseException.php
+++ ApiClient/ExactTarget/Exception/ResponseException.php
@@ -22,12 +22,16 @@ class ResponseException extends Exception
 
         $message = 'An unknown error occurred';
         if (isset($response->message)) {
-            $message = $response->message;
+            $message = sprintf('Root Message: %s', $response->message);
         } else {
             if (isset($response->results) && is_array($response->results)) {
                 foreach ($response->results as $row) {
-                    if (isset($row->ErrorMessage)) {
-                        $message = $row->ErrorMessage;
+                    if (isset($row->ErrorMessage) && !empty($row->ErrorMessage)) {
+                        $message = sprintf('Row ErrorMessage: %s', $row->ErrorMessage);
+                        break;
+                    }
+                    if (isset($row->ValueErrors)) {
+                        $message = sprintf('Row ValueErrors: %s', json_encode($row->ValueErrors));
                         break;
                     }
                 }
